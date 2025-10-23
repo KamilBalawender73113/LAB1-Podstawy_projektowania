fb038e24e3b8d5984f0ee7078a056c6d40693fa9
KamilBalawender73113 <73113@student.wsiz.edu.pl>
2025-10-23 23:12:07 +0200

    .

---
+++ zad7.py
@@ -0,0 +1,9 @@
+print("Program rozwiązujący równanie liniowe ax + b = 0")
+a = float(input("Podaj a: "))
+b = float(input("Podaj b: "))
+
+if a != 0:
+    x = -b / a
+    print(f"Rozwiązaniem równania jest x = {round(x, 2)}")
+elif b == 0:
+    print("Równanie ma nieskończenie wiele rozwiązań")
